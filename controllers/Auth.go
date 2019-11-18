ad1ad9f97f074bd63b41e19bf4a1b77924d1b2a3
Robert Wooten <robert.wooten.833@my.csun.edu>
2019-11-17 18:40:06 -0800

    Added JWT Middleware, Set up basic DB access

--- controllers/Auth.go
+++ controllers/Auth.go
@@ -1,1 +1,53 @@
 package controllers
+
+import (
+	"fmt"
+	u "goapp1/util"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/dgrijalva/jwt-go"
+)
+
+// SigningKey Holds the secret for signing jwt
+var SigningKey = []byte(os.Getenv("token_secret"))
+
+// JwtMiddleware checks requests that require auth
+func JwtMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		noAuthRequired := []string{"/api/Login", "/api/hello"}
+		requestURL := r.URL.Path
+
+		if noAuthRequired[0] == requestURL || noAuthRequired[1] == requestURL {
+			next.ServeHTTP(w, r)
+			return
+		}
+
+		tokenHeader := r.Header.Get("Authorization")
+		fmt.Println("JWT from client request", tokenHeader)
+		if tokenHeader == "" {
+			fmt.Println("no jwt")
+			response := u.Message("fail", "Missing auth token")
+			w.WriteHeader(http.StatusForbidden)
+			w.Header().Add("Content-Type", "application/json")
+			u.Response(w, response)
+			return
+		}
+		//CreateToken()
+		next.ServeHTTP(w, r)
+	})
+}
+
+// CreateToken call this to create the token using secret and return the string
+func CreateToken() string {
+	token := jwt.New(jwt.SigningMethodHS256)
+	claims := token.Claims.(jwt.MapClaims)
+	claims["user"] = true
+	claims["name"] = "Bob Smith"
+	claims["exp"] = time.Now().Add(time.Hour * 24).Unix()
+	tokenString, _ := token.SignedString(SigningKey)
+	//w.Write([]byte(tokenString))
+	fmt.Println(tokenString)
+	return tokenString
+}
--- controllers/MainEndpointController.go
+++ controllers/MainEndpointController.go
@@ -5,8 +5,23 @@ import (
 	"net/http"
 )
 
+// GetMessage returns a simple response
 func GetMessage() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		u.Response(w, u.Message("success", "it worked"))
+	}
+}
+
+// Login will take credentials, verify and send a jwt in the response
+func Login() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		u.Response(w, map[string]interface{}{"jwt": CreateToken()})
+	}
+}
+
+// GetFavoriteColor returns favorite colors
+func GetFavoriteColor(user string) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+
 	}
 }
